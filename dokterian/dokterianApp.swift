bf0bbdd7293365a443c949ae402316ac445ae8ea
Kim Cortez <kimcortez@Kims-Laptop.local>
2024-05-31 22:41:52 +0800

    Initial commit

---
+++ dokterian/dokterianApp.swift
@@ -0,0 +1,17 @@
+//
+//  dokterianApp.swift
+//  dokterian
+//
+//  Created by Kim Cortez on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct dokterianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
